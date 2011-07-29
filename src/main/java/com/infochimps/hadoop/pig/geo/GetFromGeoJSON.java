2adf0744003ca77e6edeee0df09b605d708b0eb0
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-07-29 18:09:43 +0000

    simple udf to get fields from geoJSON

---
+++ src/main/java/com/infochimps/hadoop/pig/geo/GetFromGeoJSON.java
@@ -0,0 +1,72 @@
+package com.infochimps.hadoop.pig.geo;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.TupleFactory;
+
+import org.mapfish.geo.MfGeo;
+import org.mapfish.geo.MfGeoFactory;
+import org.mapfish.geo.MfGeoJSONReader;
+import org.mapfish.geo.MfGeometry;
+import org.mapfish.geo.MfFeature;
+
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+   Given a geoJSON feature, returns the field specified.
+ */
+public class GetFromGeoJSON extends EvalFunc<String> {
+    private final MfGeoFactory mfFactory = new MfGeoFactory() {
+            public MfFeature createFeature(String id, MfGeometry geometry, JSONObject properties) {
+                return new GeoFeature(id, geometry, properties);
+            }
+        };
+
+    private final MfGeoJSONReader reader = new MfGeoJSONReader(mfFactory);
+
+    private static final String PERIOD = "\\.";
+    private static final String GEOJSON_ID = "id";
+    private static final String GEOJSON_TYPE = "type";
+    private static final String GEOJSON_FEATURE = "Feature";
+    private static final String GEOJSON_GEOM = "geometry";
+    private static final String GEOJSON_PROP = "properties";
+    
+    public String exec(Tuple input) throws IOException {
+        if (input == null || input.size() < 2 || input.isNull(0) || input.isNull(1))
+            return null;
+
+        String fieldString = input.get(0).toString();
+        String json = input.get(1).toString();
+        String result = null;
+        try {
+            GeoFeature feature = (GeoFeature)reader.decode(json);
+            if (fieldString.equals(GEOJSON_ID)) {
+                result = feature.getFeatureId();
+            } else if (fieldString.equals(GEOJSON_TYPE)) {
+                result = GEOJSON_FEATURE;
+            } else if (fieldString.equals(GEOJSON_GEOM)) {
+                result = feature.getMfGeometry().toString();
+            } else if (fieldString.startsWith(GEOJSON_PROP)){
+                String[] nestedFields = fieldString.split(PERIOD);
+                if (nestedFields.length > 1) {
+                    Object value = feature.getFromProperties(nestedFields[1]);
+                    for (int i = 2; i < nestedFields.length; i++) {
+                        value = ((HashMap<String, Object>)value).get(nestedFields[i]);
+                    }
+                    result = value.toString();
+                }                
+            } 
+
+        } catch (JSONException e) {}
+        return result;
+    }
+}
