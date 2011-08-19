7cc2b77bb3e9fe4297aaa9c4d260b6af919b56d3
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-08-19 10:50:03 -0500

    revert features_to_collection.rb back to one that actually works

--- bin/features_to_collection.rb
+++ bin/features_to_collection.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/env ruby
-features = ARGV[0].nil? ? STDIN.read.split("\n") : File.read(ARGV[0])
-puts "{'type':'FeatureCollection','features':[#{features.map { |geojson| geojson.strip}.join(',')}]}"
+
+
+collection = "{\"type\":\"FeatureCollection\", \"features\":["
+features   = []
+$stdin.each{|feature| features << feature.strip }
+collection += features.join(",")
+collection += "]}"
+puts collection
